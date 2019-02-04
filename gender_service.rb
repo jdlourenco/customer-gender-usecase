597c59460d201a908e44488a05cc692a5b0a859e
José Lourenço <joseduartelourenco@gmail.com>
2019-02-04 23:48:05 +0000

    add last changes to the gender service

--- gender_service.rb
+++ gender_service.rb
@@ -8,23 +8,43 @@ set :bind, '0.0.0.0'
 es_client = Elasticsearch::Client.new host: ENV['ES_HOST'] || 'localhost'
 
 get '/getGender/:clientId' do
-	gender = get_gender es_client, params['clientId']
+	begin
+		gender = get_gender es_client, params['clientId']
+	rescue Exception => ex
+		status 404
+		body "#{params['clientId']} not found}"
+	end
 
 	content_type :json
 	gender.to_json
 end
 
+WHEIGHTS = {
+	last_gender_visited: 1.0,
+	top_gender:          2.0,
+	top_gender_7d:       3.0
+}
+
 def get_gender es_client, clientId
 	last_gender_visited = get_last_gender_visited    es_client, clientId
 	top_gender          = get_top_gender             es_client, clientId
-	top_gender_7h       = get_top_gender_time_window es_client, clientId
+	top_gender_7d       = get_top_gender_time_window es_client, clientId
+
+	raise Exception.new unless last_gender_visited and top_gender and top_gender_7d
+
+	gender = ([
+		(last_gender_visited == 'M' ? 0 : 1) * WHEIGHTS[:last_gender_visited],
+		(top_gender          == 'M' ? 0 : 1) * WHEIGHTS[:top_gender],
+		(top_gender_7d       == 'M' ? 0 : 1) * WHEIGHTS[:top_gender_7d]
+	].sum / WHEIGHTS.values.sum).round == 0 ? 'M' : 'F'
 	
 	{
 		clientId: clientId,
+		gender: gender,
 		details: {
 			last_gender_visited: last_gender_visited,
 			top_gender:          top_gender,
-			top_gender_7h:       top_gender_7h
+			top_gender_7d:       top_gender_7d
 		}
 	}
 end
